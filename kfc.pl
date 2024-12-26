af0206ff54f41dfb4e9e212bd7851db6bc25085a
Raymond Lei(雷菩宇) <ray810815@gmail.com>
2024-12-25 23:11:31 -0500

    Create kfc.pl

---
+++ kfc.pl
@@ -0,0 +1,11 @@
+% Alternative set theories beyond ZFC and MK
+alternative_set_theory(vopenkas_ast).
+alternative_set_theory(von_neumann_bernays_godel).
+alternative_set_theory(tarski_grothendieck).
+alternative_set_theory(ackermann).
+alternative_set_theory(new_foundations).
+alternative_set_theory(positive_set_theory).
+alternative_set_theory(internal_set_theory).
+alternative_set_theory(naive_set_theory).
+alternative_set_theory(kripke_platek).
+alternative_set_theory(constructive_set_theory).
